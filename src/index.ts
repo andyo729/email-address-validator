7139441bf7eb403cf102a63981195328d65f987a
Andy Orahoske <orahoske.2@gmail.com>
2018-04-21 22:55:02 -0400

    Initial thoughts around tokenizing the email address

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import Token from './tokens/Token';
+import LocalName from './tokens/LocalName';
+
+const getLastTokenInArray = (arr: Token[]): Token => {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  return arr.splice(-1)[0];
+};
+
 export default (email: string): Boolean => {
+  const tokens = email.split('').reduce(
+    (currentTokens: Token[], currentChar: string): Token[] => {
+      const currentToken = getLastTokenInArray(currentTokens);
+      const newToken = new LocalName();
+      newToken.value = `${currentToken.value}${currentChar}`;
+      return [newToken];
+    },
+    [new LocalName()]
+  );
   return true;
 };
